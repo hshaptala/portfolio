73b767170fc926169e2f91f9b8414e07ad58679e
Hlib Shaptala <hlib.shaptala@etu.u-bordeaux.fr>
2024-05-28 15:48:55 +0200

    check

---
+++ src/helpers/skills.js
@@ -0,0 +1,33 @@
+import html from "./../img/icons/html.png";
+import css from "./../img/icons/css.png";
+import js from "./../img/icons/js.png";
+import sql from "./../img/icons/sql.png";
+import java from "./../img/icons/java.png";
+import python from "./../img/icons/python.png";
+import csharp from "./../img/icons/csharp.png";
+import c from "./../img/icons/c.png";
+import git from "./../img/icons/git.png";
+import react from "./../img/icons/react.png";
+import bash from "./../img/icons/bash.png";
+import wordpress from "./../img/icons/wordpress.png";
+
+const languages = [
+  { img: css, name: "HTML" },
+  { img: css, name: "CSS" },
+  { img: js, name: "JavaScript" },
+  { img: java, name: "Java" },
+  { img: python, name: "Python" },
+  { img: c, name: "C" },
+  { img: csharp, name: "C#" },
+  { img: sql, name: "SQL" },
+  { img: bash, name: "Bash" },
+];
+
+const tools = [
+  { img: git, name: "Git" },
+  { img: wordpress, name: "Wordpress" },
+];
+
+const frameworks = [{ img: react, name: "React" }];
+
+export { languages, tools, frameworks };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,6 @@
-import html from "./../img/icons/html.png";
-import css from "./../img/icons/css.png";
-import js from "./../img/icons/js.png";
-import sql from "./../img/icons/sql.png";
-import java from "./../img/icons/java.png";
-import python from "./../img/icons/python.png";
-import csharp from "./../img/icons/csharp.png";
-import c from "./../img/icons/c.png";
-import git from "./../img/icons/git.png";
-import react from "./../img/icons/react.png";
-import bash from "./../img/icons/bash.png";
-import wordpress from "./../img/icons/wordpress.png";
-
 import Header from "./../components/header/Header";
+
+import { languages, tools, frameworks } from "./../helpers/skills";
 
 const Home = () => {
   return (
@@ -23,66 +12,36 @@ const Home = () => {
           <div className="content-category">
             <h2>Languages</h2>
             <ul className="content-category-list">
-              <li className="content-category-list-item">
-                <img src={html} alt="HTML" />
-                <p>HTML</p>
-              </li>
-              <li className="content-category-list-item">
-                <img src={css} alt="CSS" />
-                <p>CSS</p>
-              </li>
-              <li className="content-category-list-item">
-                <img src={js} alt="JavaScript" />
-                <p>JavaScript</p>
-              </li>
-              <li className="content-category-list-item">
-                <img src={java} alt="Java" />
-                <p>Java</p>
-              </li>
-              <li className="content-category-list-item">
-                <img src={python} alt="Python" />
-                <p>Python</p>
-              </li>
-              <li className="content-category-list-item">
-                <img src={c} alt="C" />
-                <p>C</p>
-              </li>
-              <li className="content-category-list-item">
-                <img src={csharp} alt="C#" />
-                <p>C#</p>
-              </li>
-              <li className="content-category-list-item">
-                <img src={sql} alt="SQL" />
-                <p>SQL</p>
-              </li>
-
-              <li className="content-category-list-item">
-                <img src={bash} alt="Bash" />
-                <p>Bash</p>
-              </li>
+              {languages.map((skill, index) => (
+                <li key={index} className="content-category-list-item">
+                  <img src={skill.img} alt={skill.name} />
+                  <p>{skill.name}</p>
+                </li>
+              ))}
             </ul>
           </div>
 
           <div className="content-category">
             <h2>Tools</h2>
             <ul className="content-category-list">
-              <li className="content-category-list-item">
-                <img src={git} alt="Git" />
-                <p>Git</p>
-              </li>
-              <li className="content-category-list-item">
-                <img src={wordpress} alt="WordPress" />
-                <p>WordPress</p>
-              </li>
+              {tools.map((skill, index) => (
+                <li key={index} className="content-category-list-item">
+                  <img src={skill.img} alt={skill.name} />
+                  <p>{skill.name}</p>
+                </li>
+              ))}
             </ul>
           </div>
+
           <div className="content-category">
             <h2>Frameworks</h2>
             <ul className="content-category-list">
-              <li className="content-category-list-item">
-                <img src={react} alt="React" />
-                <p>React</p>
-              </li>
+              {frameworks.map((skill, index) => (
+                <li key={index} className="content-category-list-item">
+                  <img src={skill.img} alt={skill.name} />
+                  <p>{skill.name}</p>
+                </li>
+              ))}
             </ul>
           </div>
         </div>
