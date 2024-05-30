8880d3af8d1c9e0442c9277cae2427bd18935f6e
Hlib Shaptala <hlib.shaptala@etu.u-bordeaux.fr>
2024-05-30 15:52:02 +0200

    Update about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,29 +3,29 @@ import me from "./../img/about/me.jpg";
 const About = () => {
   return (
     <main className="section">
-      <div className="container">
-        <h1 className="title-1">About</h1>
-        <div className="about-content">
-          <img src={me} alt="my pic" className="my-picture" />
-          <p className="about-text">
-            Suck my balls. Suck my balls. Suck my balls. Suck my balls. Suck my
-            balls. Suck my balls. Suck my balls. Suck my balls. Suck my balls.
-            Suck my balls. Suck my balls. Suck my balls. Suck my balls. Suck my
-            balls. Suck my balls. Suck my balls. Suck my balls. Suck my balls.
-            Suck my balls. Suck my balls. Suck my balls. Suck my balls. Suck my
-            balls. Suck my balls. Suck my balls. Suck my balls. Suck my balls.
-            Suck my balls. Suck my balls. Suck my balls. Suck my balls. Suck my
-            balls. Suck my balls. Suck my balls. Suck my balls. Suck my balls.
-            Suck my balls. Suck my balls. Suck my balls. Suck my balls. Suck my
-            balls. Suck my balls. Suck my balls. Suck my balls. Suck my balls.
-            Suck my balls. Suck my balls. Suck my balls. Suck my balls. Suck my
-            balls. Suck my balls. Suck my balls. Suck my balls. Suck my balls.
-            Suck my balls. Suck my balls. Suck my balls. Suck my balls. Suck my
-            balls. Suck my balls. Suck my balls. Suck my balls. Suck my balls.
-            Suck my balls. <br></br>
-            <br></br>© Shaptala Hlib
+      <h2 className="title-1">About</h2>
+      <div className="container about-container">
+        <div className="about-text-container">
+          <h1 className="intro">
+            Hi{" "}
+            <span role="img" aria-label="wave">
+              👋
+            </span>
+            ,<br />
+            I’m Gleb,
+            <br />
+            IT Student
+          </h1>
+          <p className="about-description">
+            I'm an IT student at the University of Bordeaux. My passion for
+            Information Technologies started when I was 12 years old. I started
+            with Python. Then I learned HTML and CSS. Now I'm learning React.
+            I'm also interested in C/C++ languages. Currently, I'm looking for
+            an internship in web development.
           </p>
+          <button className="btn btn-black">See My Projects</button>
         </div>
+        <img src={me} alt="me" className="about-image" />
       </div>
     </main>
   );
