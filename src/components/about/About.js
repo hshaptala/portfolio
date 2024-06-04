b92da7ecd75948fb49b8cf55a397505dc5aab55c
Hlib Shaptala <hlib.shaptala@etu.u-bordeaux.fr>
2024-06-04 20:17:43 +0200

    up

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -5,16 +5,18 @@ const About = () => {
   return (
     <main id="about" className="section">
       <h2 className="title-1">About</h2>
-      <div className="container about-container">
-        <img src={about} alt="about" id="about-img"></img>
-        <div className="about-text-container">
-          <p className="about-description">
-            Hey, my name is Gleb, I'm studying at the University of Bordeaux. My
-            passion for Information Technologies started when I was 12 years
-            old. I started with Python. Then I learned HTML and CSS. Now I'm
-            learning React. I'm also interested in C/C++ languages. Currently,
-            I'm looking for an internship in web development.
-          </p>
+      <div className="container">
+        <div className="about">
+          <img src={about} alt="about" id="about-img"></img>
+          <div className="about-text">
+            <p>
+              Hey, my name is Gleb, I'm studying at the University of Bordeaux.
+              My passion for Information Technologies started when I was 12
+              years old. I started with Python. Then I learned HTML and CSS. Now
+              I'm learning React. I'm also interested in C/C++ languages.
+              Currently, I'm looking for an internship in web development.
+            </p>
+          </div>
         </div>
       </div>
     </main>
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,29 +5,31 @@ import me from "./../../img/about/me.jpg";
 const Header = () => {
   return (
     <main id="header" className="section">
-      <div className="container header-container">
-        <img src={me} alt="me" className="header-image" />
-        <div className="header-text-container">
-          <h1 className="intro">
-            IT Student
-            <span role="img" aria-label="wave">
-              👋
-            </span>
-          </h1>
-          <p className="header-description">
-            Hi, I'm Hlib Shaptala. An IT student based in Bordeaux, France. 📍
-          </p>
-          <div className="btn-group">
-            <button className="btn btn-black">
-              <Link to="projects" smooth={true} duration={500}>
-                See My Projects
-              </Link>
-            </button>
-            <button className="btn btn-contact">
-              <Link to="contact" smooth={true} duration={500}>
-                Contact Me
-              </Link>
-            </button>
+      <div className="container">
+        <div class="header">
+          <img src={me} alt="me" className="header-img" />
+          <div className="header-text">
+            <h1>
+              IT Student
+              <span role="img" aria-label="wave">
+                👋
+              </span>
+            </h1>
+            <p>
+              Hi, I'm Hlib Shaptala. An IT student based in Bordeaux, France. 📍
+            </p>
+            <div className="btn-group">
+              <button className="btn btn-black">
+                <Link to="projects" smooth={true} duration={500}>
+                  See My Projects
+                </Link>
+              </button>
+              <button className="btn btn-contact">
+                <Link to="contact" smooth={true} duration={500}>
+                  Contact Me
+                </Link>
+              </button>
+            </div>
           </div>
         </div>
       </div>
