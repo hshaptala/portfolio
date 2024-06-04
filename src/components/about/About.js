cc61d9a0251ba76b461718b8af2ebf9f64826712
Hlib Shaptala <hlib.shaptala@etu.u-bordeaux.fr>
2024-06-04 21:12:24 +0200

    about update

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -4,17 +4,18 @@ import about from "./../../img/about/about.jpeg";
 const About = () => {
   return (
     <main id="about" className="section">
-      <h2 className="title-1">About</h2>
       <div className="container">
         <div className="about">
           <img src={about} alt="about" id="about-img"></img>
           <div className="about-text">
+            <h3>About me</h3>
             <p>
               Hey, my name is Gleb, I'm studying at the University of Bordeaux.
-              My passion for Information Technologies started when I was 12
-              years old. I started with Python. Then I learned HTML and CSS. Now
-              I'm learning React. I'm also interested in C/C++ languages.
-              Currently, I'm looking for an internship in web development.
+              <br></br>
+              <br></br>I started with Python when I was 12. Then I learned HTML
+              and CSS. Now I'm learning React. I'm also interested in C/C++
+              languages. Currently, I'm looking for an internship in web
+              development.
             </p>
           </div>
         </div>
