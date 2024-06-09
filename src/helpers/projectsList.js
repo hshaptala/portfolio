d36dcf6a8f2414e5b6539b92c4f49b59a19d22a8
Hlib Shaptala <hlib.shaptala@etu.u-bordeaux.fr>
2024-06-09 16:10:54 +0200

    Add new projects

--- src/helpers/projectsList.js
+++ src/helpers/projectsList.js
@@ -1,11 +1,17 @@
 import towa from "./../img/projects/towa.png";
 import towaBig from "./../img/projects/towa-big.png";
 
+import pixelwar from "./../img/projects/pixelwar.png";
+import pixelwarBig from "./../img/projects/pixelwar-big.png";
+
 import odomo from "./../img/projects/odomo.png";
 import odomoBig from "./../img/projects/odomo-big.png";
 
-import pixelwar from "./../img/projects/pixelwar.png";
-import pixelwarBig from "./../img/projects/pixelwar-big.png";
+import graph from "./../img/projects/graph.png";
+import graphBig from "./../img/projects/graph-big.png";
+
+import zaloupa from "./../img/projects/zaloupa.png";
+import zaloupaBig from "./../img/projects/zaloupa-big.png";
 
 const projects = [
   {
@@ -14,16 +20,7 @@ const projects = [
     img: towa,
     imgBig: towaBig,
     skills: ["Java"],
-    link: "https://github.com/hshaptala",
-  },
-
-  {
-    title: "Pixel War IUT",
-    description: "Web pixel battle game",
-    img: pixelwar,
-    imgBig: pixelwarBig,
-    skills: ["HTML", "CSS", "JavaScript"],
-    link: "https://github.com/hshaptala",
+    link: "https://github.com/hshaptala/Towa",
   },
 
   {
@@ -32,7 +29,34 @@ const projects = [
     img: odomo,
     imgBig: odomoBig,
     skills: ["Java"],
-    link: "https://github.com/hshaptala",
+    link: "https://github.com/hshaptala/Odomo",
+  },
+
+  {
+    title: "Pixel War IUT",
+    description: "Web pixel battle game",
+    img: pixelwar,
+    imgBig: pixelwarBig,
+    skills: ["HTML", "CSS", "JavaScript"],
+    link: "https://github.com/hshaptala/Pixel-War-IUT",
+  },
+
+  {
+    title: "Graph Visualization",
+    description: "Application to visualize graphs by bundling edges",
+    img: graph,
+    imgBig: graphBig,
+    skills: ["Java"],
+    link: "https://github.com/hshaptala/Graph-Visualization",
+  },
+
+  {
+    title: "Document Signing",
+    description: "Design for a website that signs documents online",
+    img: zaloupa,
+    imgBig: zaloupaBig,
+    skills: ["HTML", "CSS"],
+    link: "https://github.com/hshaptala/Document-Signing",
   },
 ];
 
