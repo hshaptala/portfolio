69fe8ad88bb6fef3ed2bc85df6653e4a9518844d
Hlib Shaptala <hlib.shaptala@etu.u-bordeaux.fr>
2024-10-17 21:18:20 +0200

    Return old projects

--- src/helpers/projectsList.js
+++ src/helpers/projectsList.js
@@ -4,8 +4,16 @@ import pixelwarBig from "./../img/projects/pixelwar-big.png";
 import stathydre from "./../img/projects/stathydre.png";
 import stathydreBig from "./../img/projects/stathydre-big.png";
 
-const projects = [
+import graph from "./../img/projects/graph.png";
+import graphBig from "./../img/projects/graph-big.png";
 
+import zaloupa from "./../img/projects/zaloupa.png";
+import zaloupaBig from "./../img/projects/zaloupa-big.png";
+
+import towa from "./../img/projects/towa.png";
+import towaBig from "./../img/projects/towa-big.png";
+
+const projects = [
   {
     title: "Web app with RESTful API",
     description: "Pixel battle game",
@@ -26,8 +34,41 @@ const projects = [
     imgBig: stathydreBig,
     skills: ["C#"],
     link: "https://github.com/hshaptala/StatHydre",
-  }
+  },
 
+  {
+    title: "Towa",
+    description: "Board game with AI implementation",
+    fullDescription:
+      "Towa is a strategic chessboard game for 2 players. The goal is to score the most points by moving pawns. During this project, i have coded the rules as well as an AI for this game.",
+    img: towa,
+    imgBig: towaBig,
+    skills: ["Java"],
+    link: "https://github.com/hshaptala/Towa",
+  },
+
+  {
+    title: "Graph Visualization",
+    description: "Application to visualize graphs by bundling edges",
+    fullDescription:
+      "In this project I used a Java library for modeling graphs. It was necessary to program breadth-first and depth-first traversal algorithms to find the shortest path between two nodes.",
+    img: graph,
+    imgBig: graphBig,
+    skills: ["Java"],
+    link: "https://github.com/hshaptala/Graph-Visualization",
+  },
+
+  {
+    title: "Document signing website",
+    description: "Design for a website that signs documents online",
+    fullDescription:
+      "This project is a website for a company that offers online document signing. The goal was to create a design from a scratch using flexbox.",
+    img: zaloupa,
+    imgBig: zaloupaBig,
+    skills: ["HTML", "CSS"],
+    link: "https://github.com/hshaptala/Document-Signing",
+  },
+  
 ];
 
 export { projects };
